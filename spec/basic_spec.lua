525bd189feb5d6cd507c073e28f377562ac825bf
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2016-05-27 23:04:24 -0300

    Some stuff in spec

--- spec/basic_spec.lua
+++ spec/basic_spec.lua
@@ -60,7 +60,7 @@ describe("Plutfo and UTF-8 Lua Library unit testing framework", function ()
 	describe("utf8.len", function ()
 		it("Passing cases", function ()
 			assert.is.equal(utf.len(""),				0)
-			assert.is.equal(utf.len("abcdef"),			#("abcdef"))
+			assert.is.equal(utf.len("abcdef"),			6)
 			assert.is.equal(utf.len("ñábceí"),			6)
 			assert.is.equal(utf.len("ñábceí", 3),		5)
 			assert.is.equal(utf.len("ñábceí", 3, 3),	1)
@@ -97,7 +97,7 @@ describe("Plutfo and UTF-8 Lua Library unit testing framework", function ()
 				t[position] = char
 			end
 			
-			assert.are.same(t, {[1] = 97, [2] = 98, [3] = 241, [5] = 201, [7] = 194})
+			assert.are.same({[1] = 97, [2] = 98, [3] = 241, [5] = 201, [7] = 194}, t)
 		end)
 
 		describe("Invalid byte sequence", function ()
@@ -112,10 +112,10 @@ describe("Plutfo and UTF-8 Lua Library unit testing framework", function ()
 			local result	= { utf.codepoint(s, 1, #s - 1) }
 			local expected	= { 225, 233, 237 }
 
-			assert.are.same(result, expected)
+			assert.are.same(expected, result)
 
 			local zero		= #{ utf.codepoint(s, 4, 3) }
-			assert.is.equal(zero, 0)
+			assert.is.equal(0, zero)
 		end)
 
 		describe("Invalid byte sequence", function ()
@@ -132,11 +132,11 @@ describe("Plutfo and UTF-8 Lua Library unit testing framework", function ()
 	
 	describe("utf8.char", function ()
 		it("Passing cases", function ()
-			assert.is.equal(utf.char(),					""   )
-			assert.is.equal(utf.char(97, 98, 99),		"abc")
-			assert.is.equal(utf.char(225, 233, 237),	"áéí")
+			assert.is.equal("",		utf.char()				)
+			assert.is.equal("abc",	utf.char(97, 98, 99)	)
+			assert.is.equal("áéí",	utf.char(225, 233, 237)	)
 
-			assert.is.equal(utf.codepoint(utf.char(0x10FFFF)), 0x10FFFF)
+			assert.is.equal(0x10FFFF, utf.codepoint(utf.char(0x10FFFF)))
 		end)
 		
 		it("Out of range", function ()
